84ff043af1ab4efbd953cb2cf17468edad3d5f71
SpinHit <xxdylanxx2000@gmail.com>
2019-04-15 12:36:59 +0200

    Update widget.js

--- InfoNavigateurWidget-master/js/widget.js
+++ InfoNavigateurWidget-master/js/widget.js
@@ -16,7 +16,7 @@ class InfoNavigateurWidget extends Widget {
 	async ready() {
 		super.ready();
 		
-		this.controller.load();
+		
 	}
 	
 }
@@ -32,6 +32,7 @@ class InfoNavigateurModel extends WidgetModel {
 		
 	}
 
+	
 }
 
 class InfoNavigateurView extends WidgetView {
@@ -57,7 +58,7 @@ class InfoNavigateurView extends WidgetView {
 		this.stage.appendChild(Langue);
 
 		var NomNavigateur = HH.create("p");
-		NomNavigateur.innerHTML ="NomNavigateur = "+ window.navigator.userAgent + ".";
+		NomNavigateur.innerHTML =  window.os;
 		this.stage.appendChild(NomNavigateur);
 
 		var OsNavigateur = HH.create("p");
@@ -81,45 +82,119 @@ class InfoNavigateurController extends WidgetController {
 	setUp() {
 		super.setUp();
 		
-	}
+	}	
 	
-	valider() {
-		this.tableauLangue = ["francais","anglais"]; //liste contenant les langues.
-		this.mot = document.getElementById("ChampTexte").value; // variable contenant la valeur contenue dans le champ texte.
-      	       // this.mot = "prenom";
-		console.log(this.mot);
-		this.base = document.getElementById("langueBase").selectIndex; 
-        	this.baseChoix = langueBase.selectedIndex;  // variable qui contient le choix de la langue du mot
-        	this.trad = document.getElementById("langueTrad").selectIndex;
-        	this.tradChoix = langueTrad.selectedIndex; // variable qui contient le choix de langue dans lequel sera traduit le mot.
-        	//alert("[" + this.mot + "]" + " " + this.tableauLangue[this.baseChoix] + " -->" + " " + this.tableauLangue[this.tradChoix] + " " );
-		this.lien = "https://www.linguee.fr/" + this.tableauLangue[this.baseChoix] + "-" + this.tableauLangue[this.tradChoix] + "/search?source=auto&query=" + this.mot;
-		//return this.lien;
-		console.log(this.lien);
+	GetNameNav(){
+			var ua = navigator.userAgent,
+		index,
+		navigateur,
+		version;
+	if((index = ua.indexOf('Firefox'))>=0) {
+		navigateur = 'Firefox';
+		version = ua.match(/Firefox\/([0-9]+(?:\.[0-9]+)*)/)[1];
+	} else if((index = ua.indexOf('MSIE'))>=0) {
+		navigateur = 'Internet Explorer';
+		version = ua.match(/MSIE ([0-9]+(?:\.[0-9]+)*)/)[1];
+	} else if((index = ua.indexOf('Chrome'))>=0) {
+		navigateur = 'Google Chrome';
+		version = ua.match(/Chrome\/([0-9]+(?:\.[0-9]+)*)/)[1];
+	} else if((index = ua.indexOf('Opera'))>=0) {
+		navigateur = 'Opera';
+		version = ua.match(/Version\/([0-9]+(?:\.[0-9]+)*)/)[1] || ua.match(/Opera\/([0-9]+(?:\.[0-9]+)*)/)[1];
+	} else if((index = ua.indexOf('Safari'))>=0) {
+		navigateur = 'Safari';
+		version = ua.match(/Version\/([0-9]+(?:\.[0-9]+)*)/)[1] || ua.match(/Safari\/([0-9]+(?:\.[0-9]+)*)/)[1];
 	}
-		
-	async load() {
-		let link = await this.click;
-		let result = await this.mvc.main.dom(link); // load web page
-		let domstr = _atob(result.response.dom); // decode result
-		let parser = new DOMParser(); // init dom parser
-		let dom = parser.parseFromString(domstr, "text/html"); // inject result
-		this.article = new xph().doc(dom).ctx(dom).craft('//*[@id="dictionary"]/div[1]/div[1]/div[1]/div/div/div/div/div[1]/h3/span[1]/a[1]').firstResult; // find interesting things
-		this.mvc.view.update(this.article.textContent);
-		//alert(article.textContent);
-		}
-		
-		
-		
-	
+	console.log(navigateur+' '+version);
+	var r = navigateur+' '+version;
+	return r;
+	}
+
 		
 
-	
-	
-    
-	
-	
-	
-	
+
 	
 }
+
+(function () {
+	'use strict';
+	
+	var module = {
+		options: [],
+		header: [navigator.platform, navigator.userAgent, navigator.appVersion, navigator.vendor, window.opera],
+		dataos: [
+			{ name: 'Windows Phone', value: 'Windows Phone', version: 'OS' },
+			{ name: 'Windows', value: 'Win', version: 'NT' },
+			{ name: 'iPhone', value: 'iPhone', version: 'OS' },
+			{ name: 'iPad', value: 'iPad', version: 'OS' },
+			{ name: 'Kindle', value: 'Silk', version: 'Silk' },
+			{ name: 'Android', value: 'Android', version: 'Android' },
+			{ name: 'PlayBook', value: 'PlayBook', version: 'OS' },
+			{ name: 'BlackBerry', value: 'BlackBerry', version: '/' },
+			{ name: 'Macintosh', value: 'Mac', version: 'OS X' },
+			{ name: 'Linux', value: 'Linux', version: 'rv' },
+			{ name: 'Palm', value: 'Palm', version: 'PalmOS' }
+		],
+		databrowser: [
+			{ name: 'Chrome', value: 'Chrome', version: 'Chrome' },
+			{ name: 'Firefox', value: 'Firefox', version: 'Firefox' },
+			{ name: 'Safari', value: 'Safari', version: 'Version' },
+			{ name: 'Internet Explorer', value: 'MSIE', version: 'MSIE' },
+			{ name: 'Opera', value: 'Opera', version: 'Opera' },
+			{ name: 'BlackBerry', value: 'CLDC', version: 'CLDC' },
+			{ name: 'Mozilla', value: 'Mozilla', version: 'Mozilla' }
+		],
+		init: function () {
+			var agent = this.header.join(' '),
+				os = this.matchItem(agent, this.dataos),
+				browser = this.matchItem(agent, this.databrowser);
+			
+			return { os: os, browser: browser };
+		},
+		matchItem: function (string, data) {
+			var i = 0,
+				j = 0,
+				html = '',
+				regex,
+				regexv,
+				match,
+				matches,
+				version;
+			
+			for (i = 0; i < data.length; i += 1) {
+				regex = new RegExp(data[i].value, 'i');
+				match = regex.test(string);
+				if (match) {
+					regexv = new RegExp(data[i].version + '[- /:;]([\\d._]+)', 'i');
+					matches = string.match(regexv);
+					version = '';
+					if (matches) { if (matches[1]) { matches = matches[1]; } }
+					if (matches) {
+						matches = matches.split(/[._]+/);
+						for (j = 0; j < matches.length; j += 1) {
+							if (j === 0) {
+								version += matches[j] + '.';
+							} else {
+								version += matches[j];
+							}
+						}
+					} else {
+						version = '0';
+					}
+					return {
+						name: data[i].name,
+						version: parseFloat(version)
+					};
+				}
+			}
+			return { name: 'unknown', version: 0 };
+		}
+	};
+	
+	var e = module.init(),
+		debug = '';
+	
+	window.os = 'OS = ' + e.os.name + e.os.version + "." + "<p>" +
+	'Navigateur utilisé = ' + e.browser.name + ' ' + e.browser.version + "." +'<p>';
+
+}());
